8930c95da0f3521881ca913e2d8056f78f922990
SaSa <60256151+Salsaa-05@users.noreply.github.com>
2020-04-28 13:53:45 +0200

    LIst

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React , {Component} from 'react';
+import './App.css';
+import TodoItems from './Component/Additem/todoitems/Todoitems';
+import Additem from './Component/Additem/AddItem'
+class App extends Component{
+  state = {
+items : [
+{id:1,name:"Saad",age:24},
+{id:2,name:"Gregorio",age:28},
+{id:3,name:"Delpero",age:38}
+]
+  }
+
+deleteItem = (id) => {
+  let items = this.state.items.filter(item =>{
+    return item.id !== id
+  })
+ this.setState({items})
+}
+
+addItem = (item) => {
+  item.id = Math.random();
+let items = this.state.items;
+items.push(item);
+this.setState({items:items})
+}
+
+render(){
+
+ return (
+    <div className="App containar">
+     <h1 className = "text-center">List of Rrsidents</h1> 
+      <TodoItems items = {this.state.items} deleteItem = {this.deleteItem} />
+      <Additem  addItem={this.addItem}/>
+    </div>
+  );
+
+}
+
+}
+
+export default App;
