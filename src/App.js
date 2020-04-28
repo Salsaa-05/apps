866589cc0ee74476aaf57b7edd623e6524b26652
Administrator <saadhamouda95@gmail.com>
2020-04-28 11:44:40 +0200

    Todo list without style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import TodoItems from './Component/Additem/todoitems/Todoitems';
+import Additem from './Component/Additem/AddItem'
+class App extends Component{
+  state = {
+items : [
+{id:1,name:"Saad",age:22},
+{id:2,name:"ali",age:28},
+{id:3,name:"momo",age:30}
+]
+  }
 
-function App() {
-  return (
+deleteItem = (id) => {
+  let items = this.state.items.filter(item =>{
+    return item.id !== id
+  })
+ this.setState({items})
+}
+
+addItem = (item) => {
+  item.id = Math.random();
+let items = this.state.items;
+items.push(item);
+this.setState({items:items})
+}
+
+render(){
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      TodoList
+      <TodoItems items = {this.state.items} deleteItem = {this.deleteItem} />
+      <Additem  addItem={this.addItem}/>
     </div>
   );
+
+}
+
 }
 
 export default App;
---
+++ src/Component/Additem/AddItem.js
@@ -0,0 +1,37 @@
+import React , {Component} from 'react'
+import './AddItem.css'
+class AddItem extends Component {
+    state = {
+        name :'',
+        age : ''
+    }
+    handelChange = (e) => {
+      this.setState({
+        [e.target.id] : e.target.value
+      })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.addItem(this.state)
+        this.setState({
+            name: '',
+            age:''
+        })
+    }
+
+render(){
+    return(
+        <div>
+           <form onSubmit={this.handleSubmit}>
+<input type = "text" placeholder="Enter name..." id = "name" onChange={this.handelChange} value= {this.state.name} />
+<input type = "number" placeholder="Enter age..." id = "age" onChange={this.handelChange} value={this.state.age} />
+<input type = "submit" value="Add"/>
+
+           </form>
+        </div>
+    )
+}
+
+}
+export default AddItem;
