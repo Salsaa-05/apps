ba8a304bd096815bbbb0d2f9d96b9c4af13fc85c
SaSa <60256151+Salsaa-05@users.noreply.github.com>
2020-04-29 16:48:15 +0200

    add build

---
+++ build/precache-manifest.e584b2bdb7c32af7dc4a1dc731974936.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ee1419804410045f8871c8674bc30f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5f2c66f9439282847c0",
+    "url": "/static/css/main.531354fd.chunk.css"
+  },
+  {
+    "revision": "f1db5a50f8527573c0f6",
+    "url": "/static/js/2.4015213b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4015213b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f2c66f9439282847c0",
+    "url": "/static/js/main.7578664f.chunk.js"
+  },
+  {
+    "revision": "51787af5cefcce8c9dd5",
+    "url": "/static/js/runtime-main.ea689ede.js"
+  },
+  {
+    "revision": "5d8efbc4fad7533c9c5fcddcd4130b3d",
+    "url": "/static/media/graf.5d8efbc4.png"
+  }
+]);
